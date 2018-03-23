356abc665e7ac92ddfe1395e1d530ee14ec8a13f
thewinds <wind_ns@163.com>
2018-03-23 16:05:17 +0800

    完成1005 spell it right

--- practices/pat/1005_spell_it_right.cpp
+++ practices/pat/1005_spell_it_right.cpp
@@ -3,40 +3,26 @@
 using namespace std;
 
 #define N 110
+// 2018-03-23 thewinds 
+// [题目] PAT A-1005
+// 统计家谱树中各个层级无后代的节点的数目
+// [考点] 水题...
+// [心得]
+// -- 虽然是个水题但是一开始误以为是大数加法,
+// 后来发现100位加起来最大也只有 100*9 = =||
+// 然后后来因为没有考虑 0 的情况有一个测试点一直没过,
+// 所以说一定要审题细心,考虑周到
+ 
 
 int sumNum[N];
 char numStr[N];
 int numStrLen;
 char englishNum[10][10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-void printNum(){
-    int printFlag=0,first=1;
-    for(int i=N-1;i>=0;i--){
-        if(sumNum[i]!=0){
-            printFlag=1;
-        }
-        if(printFlag==1){
-            if(first!=1){
-                printf(" ");
-            }else{
-                first=0;
-            }
-            printf("%s",englishNum[sumNum[i]]);
-        }
-    }
-    printf("\n");
-}
-
-void add(int digit){
-    int i=0,carry=digit;
-    while(carry>0){
-        int c=(sumNum[i]+carry)/10;
-        sumNum[i]=(sumNum[i]+carry)%10;        
-        carry=c;
-        i++;
-    }
-}
 
 void print(int num){
+    if (num==0){
+        printf("%s",englishNum[0]);                
+    }
     int printNum[3]={0},first=1,i=0;
     while(num>0){
         printNum[i]=num%10;
