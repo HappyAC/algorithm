a965ddae32adb9995ec978d1a616b4760fafeaea
thewinds <wind_ns@163.com>
2018-08-24 22:27:18 +0800

    impl tree

--- practices/data_structrue/tree.cpp
+++ practices/data_structrue/tree.cpp
@@ -40,50 +40,91 @@ void Tree::Assgin(char* str){
     assgin(root,str);
 }
 void Tree::PreOrder(){
+    printf("pre oreder recursion: ");
     preOrder(root);
     printf("\n");
-}
-void Tree::InOrder(){
-    inOrder(root);
-    printf("\n");
-}
-void Tree::PostOrder(){
-    printf("recursion: ");
-    postOrder(root);
-    printf("\n");
-    printf("non-recursion: ");
-    //        1
-    //     2     3
-    //   4   #  6  5
-    // #  #      #   #
-
-    // s1 1 2 4
-    // s2 1 3 5 6 2 4
-
+    printf("pre oreder non-recursion: ");
     std::stack<TreeNode*> nodes;
     nodes.push(root);
-    TreeNode *pre = NULL;
+    
     while(!nodes.empty()){
-        TreeNode *node = nodes.top();
-        if(node->left==NULL&&node->right==NULL){
-            pre=node;
-            printf("%c",node->data);
-            nodes.pop();
-            continue;
-        }
+        TreeNode *cur = nodes.top();
+        nodes.pop();
+        if (cur!=NULL) {
+            printf("%c ",cur->data);
+            nodes.push(cur->right);
+            nodes.push(cur->left);
+        } 
+    }
+    printf("\n");
+    
+}
+void Tree::InOrder(){
+    printf("in oreder recursion: ");
+    inOrder(root);
+    printf("\n");
+    printf("in oreder non-recursion: ");
+    
+    std::stack<TreeNode*> nodes;
+    TreeNode *pre = new TreeNode;
+    nodes.push(root);
 
-        if(pre==node->left){
-            nodes.push(node->right);
-            pre=node;
+    //  M
+    // L R  R=>M=>L
+    while(!nodes.empty()){
+        TreeNode *cur = nodes.top();
+        nodes.pop();
+
+        if(cur==NULL){
+            pre=cur;
             continue;
         }
         
-        if(pre==node->right){
-            pre=node;
-            printf("%c",node->data);
-            nodes.pop();
+        if (cur->left==pre||cur->right==pre) {
+            printf("%c ",cur->data);
+            pre=cur;
+            continue;
         }
+            
+        nodes.push(cur->right);
+        nodes.push(cur);
+        nodes.push(cur->left);
+        pre=cur;
     }
+
+    printf("\n");
+    
+
+}
+void Tree::PostOrder(){
+    printf("post oreder recursion: ");
+    postOrder(root);
+    printf("\n");
+    printf("post oreder non-recursion: ");
+
+    std::stack<TreeNode*> nodes;
+    nodes.push(root);
+    TreeNode *pre = new TreeNode;
+    while(!nodes.empty()){
+        TreeNode *cur = nodes.top();
+        if(cur==NULL){
+            pre=NULL;
+            nodes.pop();
+            continue;
+        }
+        if((pre==cur->left&&cur->right==NULL)
+            ||pre==cur->right){
+            printf("%c ",cur->data);
+            nodes.pop();
+        }else{
+            nodes.push(cur->right);
+            nodes.push(cur->left);
+        }
+        pre=cur;
+
+    }
+    printf("\n");
+
 }
 void Tree::assgin(TreeNode *&node,char *&str){
     if(strlen(str)==0) return;
@@ -102,21 +143,21 @@ void Tree::assgin(TreeNode *&node,char *&str){
 
 void Tree::preOrder(TreeNode *node){
     if(node==NULL) return;
-    printf("%c",node->data);  
+    printf("%c ",node->data);  
     preOrder(node->left);
     preOrder(node->right);
 }
 void Tree::inOrder(TreeNode *node){
     if(node==NULL) return;
     inOrder(node->left);
-    printf("%c",node->data);  
+    printf("%c ",node->data);  
     inOrder(node->right);
 }
 void Tree::postOrder(TreeNode *node){
     if(node==NULL) return;
     postOrder(node->left);
     postOrder(node->right);
-    printf("%c",node->data);  
+    printf("%c ",node->data);  
     
 }
 
@@ -126,21 +167,43 @@ void Tree::LevelOrder(){
     while(!nodes.empty()){
         TreeNode* node = nodes.front();
         nodes.pop();
-        printf("%c",node->data);
+        printf("%c ",node->data);
         if(node->left!=NULL) nodes.push(node->left);
         if(node->right!=NULL) nodes.push(node->right);
     }
     printf("\n");
 }
 
+int countTreeDeep(Tree tree){
+    int maxDeep=0,deep = 0;
+    std::stack<TreeNode*> nodes;
+    nodes.push(tree.root);
+    while(!nodes.empty()){
+        TreeNode *cur = nodes.top();
+        nodes.pop();
+        if(cur!=NULL){
+            printf("%c",cur->data);
+            nodes.push(cur->right);
+            nodes.push(cur->left);
+            if(cur->left!=NULL||cur->right!=NULL){
+                maxDeep++;
+            }
+        }
+    }
+    printf("\n");
+    return maxDeep;
+}
+
 int main(int argc, char const *argv[])
 {
     Tree tree;
-    char str[]="124###3#5##";
+    char str[]="1246####3#5##";
+    // char str[]="1##";
     tree.Assgin(str);
     tree.PreOrder();
     tree.InOrder();
     tree.PostOrder();
     tree.LevelOrder();
+    //printf("deep: %d",countTreeDeep(tree));
     return 0;
 }
